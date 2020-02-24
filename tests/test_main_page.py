9c8ba068d0927e44b5768a8f9637cb01a012652d
eshenko <alexeyeschenko@gmail.com>
2020-02-24 23:06:00 +0300

    create test_main_page.py

---
+++ tests/test_main_page.py
@@ -0,0 +1,7 @@
+def test_admin_login_page(browser, base_url):
+    browser.get(base_url + '/opencart/index.php?route=common/home')
+    browser.find_element_by_css_selector('div#slideshow0')
+    browser.find_element_by_css_selector('.swiper-pagination.slideshow0')
+    browser.find_element_by_xpath("//h3[contains(text(), 'Featured')]")
+    browser.find_element_by_css_selector('div.swiper-viewport')
+    browser.find_element_by_css_selector('.swiper-pagination.carousel0')
