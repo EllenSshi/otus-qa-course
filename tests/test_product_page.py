3bd28f213bb6a0420e68c7b589879e018b3cdd48
eshenko <alexeyeschenko@gmail.com>
2020-02-27 22:42:29 +0300

    add two tests to test_product_page.py with waits, EC and asserts

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,4 +1,8 @@
+import random
 from pages.locators import ProductPageLocators
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def test_product_page(browser, base_url):
@@ -9,3 +13,37 @@ def test_product_page(browser, base_url):
     browser.find_element(*ProductPageLocators.QUANTITY_INPUT)
     browser.find_element(*ProductPageLocators.ADD_TO_CART_BTN)
     browser.find_element(*ProductPageLocators.RATING)
+
+
+def test_send_review(browser, base_url):
+    browser.get(base_url + '/opencart/index.php?route=product/product&product_id=40')
+    explicit_wait = WebDriverWait(browser, 5)
+    review_tab = browser.find_element(*ProductPageLocators.REVIEW_TAB)
+    review_tab.click()
+    review_name_input = explicit_wait.until(
+        EC.visibility_of_element_located(ProductPageLocators.REVIEW_NAME_INPUT)
+    )
+    review_name_input.send_keys("Name")
+    review_text_input = explicit_wait.until(
+        EC.visibility_of_element_located(ProductPageLocators.REVIEW_TEXT_INPUT)
+    )
+    review_text_input.send_keys("This is my first review ever")
+    rating = random.randint(0, 4)
+    review_rating = browser.find_elements(*ProductPageLocators.REVIEW_RATING_MARKS)[rating]
+    review_rating.click()
+    browser.find_element(*ProductPageLocators.REVIEW_BTN).click()
+    alert = browser.find_elements(*ProductPageLocators.ALERTS)[0]
+
+    assert alert.text == "Thank you for your review. It has been submitted to the webmaster for approval."
+
+
+def test_add_product_to_cart(browser, base_url):
+    browser.get(base_url + '/opencart/index.php?route=product/product&product_id=40')
+    quantity = browser.find_element(*ProductPageLocators.QUANTITY_INPUT)
+    quantity.send_keys(Keys.BACKSPACE)
+    quantity.send_keys(3)
+    btn = browser.find_element(*ProductPageLocators.ADD_TO_CART_BTN)
+    btn.click()
+    alert = browser.find_elements(*ProductPageLocators.ALERTS)[0]
+
+    assert "Success" in alert.text
