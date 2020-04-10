b6cd5ed35692d7fec614f62a3c44ea842dbb1cdc
eshenko <alexeyeschenko@gmail.com>
2020-04-10 18:54:33 +0300

    rewrite test_catalog_page.py.py using pageObject pattern

--- pages/CatalogPage.py
+++ pages/CatalogPage.py
@@ -1,2 +1,14 @@
-class CatalogPage:
-    pass
+from .BasePage import BasePage
+from pages.locators import CatalogPageLocators
+
+
+class CatalogPage(BasePage, CatalogPageLocators):
+    path = '/opencart/index.php?route=product/category&path=20'
+
+    def find_elements(self):
+        self._wait_for_visibility(self.LIST_GROUP)
+        self._wait_for_visibility(self.LIST_BTN)
+        self._wait_for_visibility(self.GRID_BTN)
+        self._wait_for_visibility(self.COMPARE_LINK)
+        self._wait_for_visibility(self.SORT_INPUT)
+        self._wait_for_visibility(self.SHOW_INPUT)
